5ed9bff1fe149e436067f7327ea9561fe8902dd0
Edwin Jorge King <71462766+Raulmasato@users.noreply.github.com>
2025-07-09 12:53:13 -0300

    Commitear esto

---
+++ Patrones/Composite/Directorio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patrones.Composite
+{
+    class Directorio : ComponenteBase
+    {
+        private List<ComponenteBase> hijos = new List<ComponenteBase>();
+        public Directorio(string nombre) : base(nombre) { }
+        public void Agregar(ComponenteBase componente)
+        {
+            hijos.Add(componente);
+        }
+        public override void Mostrar(int indentacion = 0)
+        {
+            Console.WriteLine(new string('-', indentacion) + "Directorio " + Nombre);
+            foreach (var hijo in hijos)
+            {
+                //En definitiva, es un método recursivo
+                //Se evidencia el uso del patrón, imprimiento, elementos hijos compuestos (Directorios)
+                //o hijos de tipo hoja Archivos
+                hijo.Mostrar(indentacion + 2);
+            }
+        }
+    }
+}
