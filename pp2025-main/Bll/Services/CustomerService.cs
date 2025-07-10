7a165c8ce6c24f4e3a4ef70baa488884ec5168eb
Edwin Jorge King <71462766+Raulmasato@users.noreply.github.com>
2025-07-10 09:39:34 -0300

    removi el error y puse el repo de weingand entero

---
+++ pp2025-main/Bll/Services/CustomerService.cs
@@ -0,0 +1,66 @@
+﻿using Bll.Interfaces;
+using Dal.Factory;
+using Dal.Interfaces;
+using DomainModel;
+using Services.Facade.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bll.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        ICustomerRepository repository = Repository.GetCustomerInstance();
+
+        public CustomerService()
+        {
+            
+        }
+
+        public void Add(Customer entity)
+        {
+            repository.Add(entity);
+        }
+
+        public void Delete(Guid id)
+        {
+            repository.Delete(id);
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            return repository.GetAll();
+        }
+
+        public Customer GetById(Guid id)
+        {
+            return repository.GetById(id);
+        }
+
+        public bool IsActive(Customer customer)
+        {
+            //Desde el negocio validar por ejemplo cuándo un cliente es activo o no?
+
+            //Customer getCustomer = repository.GetById(customer.IdCustomer);
+
+            //return getCustomer.Active && getCustomer.FechaAlgo < DateTime
+
+            string palabra = "bienvenidos".Traducir();
+            
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+
+            palabra = "bienvenidos".Traducir();
+
+            return false;
+        }
+
+        public void Update(Customer entity)
+        {
+            repository.Update(entity);
+        }
+    }
+}
---
+++ pp2025-main/WinUI/WinForms/Login.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WinUI
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblContraseña = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.lnkRecuperacion = new System.Windows.Forms.LinkLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuVentas = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmGestionVentas = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmNuevaVenta = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(76, 109);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(68, 20);
+            this.lblUsuario.TabIndex = 0;
+            this.lblUsuario.Text = "Usuario:";
+            // 
+            // lblContraseña
+            // 
+            this.lblContraseña.AutoSize = true;
+            this.lblContraseña.Location = new System.Drawing.Point(76, 202);
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(96, 20);
+            this.lblContraseña.TabIndex = 1;
+            this.lblContraseña.Text = "Contraseña:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(194, 106);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(207, 26);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(194, 199);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.PasswordChar = '*';
+            this.txtContraseña.Size = new System.Drawing.Size(207, 26);
+            this.txtContraseña.TabIndex = 3;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(191, 288);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(162, 39);
+            this.btnIniciar.TabIndex = 4;
+            this.btnIniciar.Text = "Iniciar sesión";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // lnkRecuperacion
+            // 
+            this.lnkRecuperacion.AutoSize = true;
+            this.lnkRecuperacion.Location = new System.Drawing.Point(178, 367);
+            this.lnkRecuperacion.Name = "lnkRecuperacion";
+            this.lnkRecuperacion.Size = new System.Drawing.Size(175, 20);
+            this.lnkRecuperacion.TabIndex = 5;
+            this.lnkRecuperacion.TabStop = true;
+            this.lnkRecuperacion.Text = "¿Olvidó su contraseña?";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuVentas});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(548, 33);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuVentas
+            // 
+            this.mnuVentas.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmGestionVentas,
+            this.tsmNuevaVenta});
+            this.mnuVentas.Name = "mnuVentas";
+            this.mnuVentas.Size = new System.Drawing.Size(80, 29);
+            this.mnuVentas.Text = "Ventas";
+            // 
+            // tsmGestionVentas
+            // 
+            this.tsmGestionVentas.Name = "tsmGestionVentas";
+            this.tsmGestionVentas.Size = new System.Drawing.Size(270, 34);
+            this.tsmGestionVentas.Text = "Gestión";
+            // 
+            // tsmNuevaVenta
+            // 
+            this.tsmNuevaVenta.Name = "tsmNuevaVenta";
+            this.tsmNuevaVenta.Size = new System.Drawing.Size(270, 34);
+            this.tsmNuevaVenta.Text = "Nueva";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 418);
+            this.Controls.Add(this.lnkRecuperacion);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblContraseña);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblContraseña;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.LinkLabel lnkRecuperacion;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuVentas;
+        private System.Windows.Forms.ToolStripMenuItem tsmGestionVentas;
+        private System.Windows.Forms.ToolStripMenuItem tsmNuevaVenta;
+    }
+}
