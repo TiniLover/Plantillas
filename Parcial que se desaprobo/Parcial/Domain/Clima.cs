d5e0ff1507b660ed40808e26d1eb5c60e6e71cff
Edwin Jorge King <71462766+Raulmasato@users.noreply.github.com>
2025-07-10 10:01:20 -0300

    agregue parcial que desaprobamos

---
+++ Parcial que se desaprobo/Parcial/Domain/Clima.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial.Domain
+{
+    internal class Clima
+    {
+        public DateTime Fecha { get; set; }
+
+        //Monitoreamos la temperatura...
+        public int Temperatura { get; set; }
+
+        public Clima(int temperatura)
+        {
+            this.Temperatura= temperatura;
+        }
+    }
+}
